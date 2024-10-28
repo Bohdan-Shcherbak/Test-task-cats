0f890ba6279fe479de77192b0a9af1531a28dfa8
Bohdan-Shcherbak <Bohdansherbak@gmail.com>
2024-10-29 01:19:27 +0200

    test-task-cats

---
+++ js/app.js
@@ -0,0 +1,37 @@
+(() => {
+    "use strict";
+    function isWebp() {
+        function testWebP(callback) {
+            let webP = new Image;
+            webP.onload = webP.onerror = function() {
+                callback(webP.height == 2);
+            };
+            webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+        }
+        testWebP((function(support) {
+            let className = support === true ? "webp" : "no-webp";
+            document.documentElement.classList.add(className);
+        }));
+    }
+    const butt = document.querySelector(".icon-menu");
+    const menuBurger = document.querySelector(".menu-burger");
+    const burgerMenu = document.querySelector(".burger-menu");
+    console.log(butt);
+    console.log(burgerMenu);
+    butt.addEventListener("click", (() => {
+        burgerMenu.classList.toggle("active");
+        setTimeout(menuBurger.classList.toggle("active"), 3e3);
+        butt.classList.toggle("menu-open");
+    }));
+    let addWindowScrollEvent = false;
+    setTimeout((() => {
+        if (addWindowScrollEvent) {
+            let windowScroll = new Event("windowScroll");
+            window.addEventListener("scroll", (function(e) {
+                document.dispatchEvent(windowScroll);
+            }));
+        }
+    }), 0);
+    window["FLS"] = true;
+    isWebp();
+})();
